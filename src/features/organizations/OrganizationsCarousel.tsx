994ae9f30f1c464e667bb4f19819449f49f3c0b1
Felipe Lino <felipegadelha2004@gmail.com>
2026-08-12 13:59:07 -0300

    feat: adding svg background

--- src/features/organizations/OrganizationsCarousel.tsx
+++ src/features/organizations/OrganizationsCarousel.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import data from "./data/data.json";
+import hero from "@/assets/hero-background.svg";
 import logo from "@/assets/solo_logo.png";
 import Autoplay from "embla-carousel-autoplay";
 import {
@@ -30,7 +31,22 @@ const stats = [
 
 export default function OrganizationsCarousel() {
   return (
-    <div className="relative flex w-full flex-col items-center gap-8 overflow-hidden bg-gray-200 px-4 py-10 sm:px-6 lg:grid lg:grid-cols-[1fr_auto_1fr] lg:items-center lg:justify-normal lg:gap-x-16 lg:gap-y-0 lg:px-16 lg:py-0">
+    <div className="relative flex w-full flex-col items-center gap-8 overflow-hidden bg-[#FAF8FF] px-4 py-10 sm:px-6 lg:grid lg:grid-cols-[1fr_auto_1fr] lg:items-center lg:justify-normal lg:gap-x-16 lg:gap-y-0 lg:px-16 lg:py-0">
+      {/* Background */}
+      <img
+        src={hero}
+        alt=""
+        aria-hidden="true"
+        className="
+      pointer-events-none
+      absolute
+      inset-0
+      z-0
+      h-full
+      w-full
+      object-cover
+    "
+      />
       <Carousel
         opts={{ align: "start" }}
         className="order-2 w-full max-w-[500px] min-w-0 shrink-0 lg:order-none lg:max-w-[600px] lg:justify-self-end"
@@ -56,9 +72,9 @@ export default function OrganizationsCarousel() {
       </Carousel>
       <img
         src={logo}
-        className="order-1 h-auto w-[100px] shrink-0 sm:w-[120px] lg:order-none lg:w-[320px] lg:justify-self-center"
+        className="relative z-10 order-1 h-auto w-[100px] shrink-0 sm:w-[120px] lg:order-none lg:w-[320px] lg:justify-self-center"
       />
-      <div className="order-3 grid w-full max-w-[500px] grid-cols-1 gap-3 sm:grid-cols-3 lg:flex lg:w-[366px] lg:max-w-none lg:flex-col lg:justify-self-start">
+      <div className="relative z-10 order-3 grid w-full max-w-[500px] grid-cols-1 gap-3 sm:grid-cols-3 lg:flex lg:w-[366px] lg:max-w-none lg:flex-col lg:justify-self-start">
         {stats.map((stat) => (
           <div
             key={stat.label}
