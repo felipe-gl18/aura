42b462655a6bf14b896e4a3930add7fe3638d9aa
Felipe Lino <felipegadelha2004@gmail.com>
2026-08-11 21:32:44 -0300

    feat: adding scroll to top behavior through component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import ChatBotWrapper from "@/features/chatbot/ChatBotWrapper";
+import ScrollToTop from "../ScrollToTop";
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
+      <ScrollToTop />
       <Navbar />
 
       <main className="flex-1 bg-white">
